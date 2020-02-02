6a9ddd3213745befa391f91d34c0b76541c0ffab
Adeeb Siddiqui <adeeb.bd@gmail.com>
2020-02-01 23:08:28 -0600

    Added Stream Reduction examples

---
+++ src/com/lambda/apps/stream/ReductionInStream.java
@@ -0,0 +1,27 @@
+package com.lambda.apps.stream;
+
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class ReductionInStream {
+
+    public static void main(String[] args) {
+        IntStream intStream = IntStream.of(1, 2, 3, 4, 5);
+
+        int sum = intStream.sum();
+        System.out.println("Sum = " + sum);
+
+        //intStream = IntStream.of(1, 2, 3, 4, 5); //observe the error
+        OptionalInt optionalInt = intStream.reduce((x, y) -> x + y);
+        System.out.println("Sum using reduce = " + optionalInt.getAsInt());
+
+        //intStream = IntStream.of(1, 2, 3, 4, 5); //see the cause of error below
+        int sumWithIdentity = intStream.reduce(5, (x, y) -> x + y);
+        System.out.println("Sum using reduce with identity = " + sumWithIdentity);
+        /** Reduce with identity starts with identity value as X and first value of stream as Y in the accumulator function
+         * The result of the accumulator function is then set as X and second value of stream as Y and so on...*/
+    }
+}
+/**
+ * Reduction is a Terminal Operation. After Terminal Operation is performed on a Stream it is closed, no additional operation can be performed on it
+ */
