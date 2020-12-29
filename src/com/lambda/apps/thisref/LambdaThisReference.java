e4b919b97edf5e6f5f5e501b3f9ee26baf4044df
Adeeb Siddiqui <adeeb.bd@gmail.com>
2020-12-29 12:00:34 -0600

    Made changes in LambdaThisReference.java

--- src/com/lambda/apps/thisref/LambdaThisReference.java
+++ src/com/lambda/apps/thisref/LambdaThisReference.java
@@ -2,35 +2,40 @@ package com.lambda.apps.thisref;
 
 public class LambdaThisReference {
 
-	public static void main(String[] args) {
-		
-		LambdaThisReference lambda = new LambdaThisReference();
-		
-		lambda.giveHomeworkToStudent(new Student() {
-			@Override
-			public void doHomework() {
-				System.out.println("Doing math homework");
-				System.out.println(this); //this refers to the instance of anonymous Student implementation, overrides the default
-			}
-		});
-		
-		lambda.giveHomeworkToStudent( () -> {
-			System.out.println("Doing physics homework");
-			//System.out.println(this); //won't work because lambda expressions actually are NOT implementation of any interface
-		});
-		
-		lambda.prepareHomework();
-	}
-	
-	
-	public void giveHomeworkToStudent(Student student) {
-		student.doHomework();
-	}
-	
-	public void prepareHomework() {
-		giveHomeworkToStudent(() -> {
-			System.out.println("Doing biology homework");
-			System.out.println(this); //this refers to the same instance referred outside of lambda expression i.e. the instance on which prepareHomework() was called
-		});
-	}
+    public static void main(String[] args) {
+
+        LambdaThisReference lambda1 = new LambdaThisReference();
+        System.out.println("lambda1 = " + lambda1);
+
+        LambdaThisReference lambda2 = new LambdaThisReference();
+        System.out.println("lambda2 = " + lambda2);
+
+        lambda1.giveHomeworkToStudent(new Student() {
+            @Override
+            public void doHomework() {
+                System.out.println("Doing math homework");
+                System.out.println(this); //In anonymous inner class, 'this' refers to the anonymous class instance
+            }
+        });
+
+        lambda1.giveHomeworkToStudent(() -> {
+            System.out.println("Doing physics homework");
+            //System.out.println(this); //In Lambda, 'this' refers to the enclosing instance. In this case, there is no enclosing instance since main has static context
+        });
+
+        lambda1.prepareHomework();
+        lambda2.prepareHomework();
+    }
+
+
+    public void giveHomeworkToStudent(Student student) {
+        student.doHomework();
+    }
+
+    public void prepareHomework() {
+        giveHomeworkToStudent(() -> {
+            System.out.println("Doing biology homework");
+            System.out.println(this); //In Lambda, 'this' refers to the enclosing instance. In this case, the instance on which prepareHomework() is called
+        });
+    }
 }
