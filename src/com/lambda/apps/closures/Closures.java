5b9d978d7230055df4cf06f84485b34ea4f4c080
Adeeb Siddiqui <adeeb.bd@gmail.com>
2021-06-12 15:13:55 -0500

    Modified Closures.java

--- src/com/lambda/apps/closures/Closures.java
+++ src/com/lambda/apps/closures/Closures.java
@@ -6,19 +6,13 @@ public class Closures {
 		int a = 10;
 		int b = 20;
 		
-		doProcess(a, new Process() {
-			@Override
-			public void process(int i) {
-				System.out.println(i+b); 
-			}
-		});
-		
-		//b = 9;
-		/* won't work since b is 'effectively final' because at runtime process() is executed inside doProcees() where b is out of scope.
-		 * JRE keeps track of b inside main() and makes it 'effectively final' --> Closures phenomenon
-		 */
-		
 		doProcess( a, x -> System.out.println(x+b) );
+
+		b = 9;
+		/* won't work since b is 'effectively final'.
+		JRE keeps track of any local variables inside main() which are used in lambda expressions and makes it 'effectively final' --> Closure phenomenon
+		Closure has immutable state
+		https://www.youtube.com/watch?v=15X0qFtBqiQ&t=2023s */
 	}
 	
 	public static void doProcess(int i, Process p) {
