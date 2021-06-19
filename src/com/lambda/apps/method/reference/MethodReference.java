a3864ffcd696c38e456baaf8dc41fafce9d8f6b4
Adeeb Siddiqui <adeeb.bd@gmail.com>
2021-06-19 13:50:34 -0500

    Modified method reference examples

--- src/com/lambda/apps/method/reference/MethodReference.java
+++ src/com/lambda/apps/method/reference/MethodReference.java
@@ -4,8 +4,7 @@ import java.util.function.IntBinaryOperator;
 import java.util.function.IntFunction;
 
 /**
- * When a Lambda Expression calls an existing method then Method Reference can be used instead of Lambda Expression,
- *	iff the argument and return type of both Lambda Expression and the calling method are exactly the same
+ * If all a Lambda Expression does is call an existing method then Method Reference can be used instead of Lambda Expression
  */
 public class MethodReference {
 
@@ -26,6 +25,8 @@ public class MethodReference {
 		System.out.println(value);
 		
 	}
+
+	/** another example in Reduction.java */
 
 	public static void doProcess() {
 		System.out.println("No arg method reference");
--- src/com/lambda/apps/stream/Reduction.java
+++ src/com/lambda/apps/stream/Reduction.java
@@ -12,11 +12,11 @@ public class Reduction {
         System.out.println("Sum = " + sum);
 
         //intStream = IntStream.of(1, 2, 3, 4, 5); //observe the error
-        OptionalInt optionalInt = intStream.reduce((x, y) -> x + y);
+        OptionalInt optionalInt = intStream.reduce((x, y) -> Integer.sum(x, y));
         System.out.println("Sum using reduce = " + optionalInt.getAsInt());
 
         //intStream = IntStream.of(1, 2, 3, 4, 5); //see the cause of error below
-        int sumWithIdentity = intStream.reduce(5, (x, y) -> x + y);
+        int sumWithIdentity = intStream.reduce(5, Integer::sum);
         System.out.println("Sum using reduce with identity = " + sumWithIdentity);
         /** Reduce with identity starts with identity value as X and first value of stream as Y in the accumulator function
          * The result of the accumulator function is then set as X and second value of stream as Y and so on...*/
